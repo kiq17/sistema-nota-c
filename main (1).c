5b3b6ba56a16297ec7ed66bc91b03fb2737ac835
Caique <101359008+kiq17@users.noreply.github.com>
2022-09-15 17:51:01 -0300

    subindo

---
+++ main (1).c
@@ -0,0 +1,82 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int notas[] = {8,5,6,2,3,10,8,9,3,0};
+int tam = sizeof(notas) / sizeof(int) - 1;
+
+int menu();
+int opcao;
+int opcoes();
+
+int main()
+{
+    menu();
+
+    return 0;
+}
+
+
+int menu(){
+    printf("1 - Média da turma \n");
+    printf("2 - Maior Nota \n");
+    printf("3 - Menor Nota \n");
+    printf("4 - Quantidade de notas maior que 6 \n");
+    printf("5 - Finalizar programa \n");
+    scanf("%d", &opcao);
+    
+    if(opcao == 5){
+        printf("Sistema encerrado");
+    } else{
+        opcoes();
+    }
+}
+
+int opcoes(){
+    int total = 0;
+    int i;
+    int resultado;
+    
+    if(opcao == 1){
+        for(i=0; i < tam; i++){
+            total += notas[i];
+        }
+
+        resultado = total / tam;
+
+        printf("Média da turma: ");
+        printf("%d \n", resultado);
+        
+        menu();
+                
+    } else if (opcao == 2){
+        int aux = 0;
+        
+        for(i=0; i < tam; i++){
+            if(notas[i] > notas[i + 1]){
+                aux = notas[i];
+            } else if (aux < notas[i]){
+                aux = notas[i];
+            }
+        }
+        
+        printf("%d \n", aux);
+        
+    } else if (opcao == 3){
+        
+    } else if (opcao == 4){
+        
+    } else if(opcao == 5){
+        
+    } else{
+        printf("Opção inválida. \n");
+        menu();
+    }
+}
+
